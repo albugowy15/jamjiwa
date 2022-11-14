2c4963d28a876236fb7121d3acb0a44c9a65e957
albugowy15 <kholidbughowi@gmail.com>
2022-11-14 22:22:37 +0700

    add layout, header, footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,3 @@
-import { GetServerSideProps } from "next";
-import { prisma } from "db/prisma";
-import { Daerah } from "@interfaces/database";
-
-export default function Home({ daerah }: { daerah: Daerah[] }) {
-	return (
-		<div>
-			{daerah?.map((data) => (
-				<p key={data.idDaerah}>{data.namaDaerah}</p>
-			))}
-		</div>
-	);
+export default function Home() {
+	return <div>body</div>;
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-	const daerah = await prisma.daerah.findMany();
-
-	return {
-		props: {
-			daerah: JSON.parse(JSON.stringify(daerah)),
-		},
-	};
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgPrimary: "#28293D",
+        bgSecondary: "#2D2F45",
+        bgCard: "#373951",
+        btnPrimary: "rgba(255, 255, 255, 0.1)",
+        btnSecondary: "#3643FC",
+        onSurface: "#50527A"
+      },
+      fontFamily: {
+        inter: ["Inter", ...fontFamily.sans],
+        rFlex: ["Roboto Flex", ...fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
